b92f223cc24dde375c2ffa2d64a4280067c282fc
Matthias Noback <matthiasnoback@gmail.com>
2019-01-29 12:49:20 +0100

    Upgrade dependencies for PHP 7.2

--- run_tests.sh
+++ run_tests.sh
@@ -8,20 +8,17 @@ HOST_GID=$(id -g)
 mkdir -p build/logs/
 docker-compose up -d mysql
 
-docker-compose run --user="${HOST_UID}:${HOST_GID}" php56 \
+docker-compose run --user="${HOST_UID}:${HOST_GID}" php72 \
     composer install --prefer-dist
 
-docker-compose run --user="${HOST_UID}:${HOST_GID}" php56 \
+docker-compose run --user="${HOST_UID}:${HOST_GID}" php72 \
     vendor/bin/phpunit -v \
     --coverage-text \
     --coverage-clover=build/logs/clover.xml
 
-docker-compose run --user="${HOST_UID}:${HOST_GID}" php72 \
-    vendor/bin/phpunit -v
-
 docker-compose run phpstan analyze
 
-docker-compose run --user="${HOST_UID}:${HOST_GID}" php56 \
+docker-compose run --user="${HOST_UID}:${HOST_GID}" php72 \
     rm -r build/migrations/ || true && \
     php test/TalisOrm/DoctrineMigrations/doctrine-migrations.php migrations:diff | grep "Generated new migration class" && \
     php test/TalisOrm/DoctrineMigrations/doctrine-migrations.php migrations:migrate --no-interaction && \
